b06a925ed1159b9159b47a6aa4c01760aa3ab0c9
Blergborg <pprater92@gmail.com>
2023-09-13 15:03:32 -0700

    Updated ContentView NavigationLinks to use MissionView() instead of Text() Added init{} to MissionView that maps crew property of Missions to individual Astronaut data. Added Horizontal scroll row to show CrewMember information for a Mission to MissionView. Updated styling in MissionView to separate sections with a custom Divider made from a Rectangle.

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -7,8 +7,32 @@
 
 import SwiftUI
 
+
+
 struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+
     let mission: Mission
+    let crew: [CrewMember]
+    
+    // we need to assemble the codable data we get from 2 different sources.
+    // need to through fatalError if data between the 2 sources doesn't match up
+    // however, if it does we can merge the Codable data into the interal CrewMember struct
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -21,11 +45,58 @@ struct MissionView: View {
                         .padding(.top)
                     
                     VStack(alignment: .leading) {
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(crew, id: \.role) { crewMember in
+                                    NavigationLink {
+                                        Text("Astronaut details")
+                                    } label: {
+                                        HStack {
+                                            Image(crewMember.astronaut.id)
+                                                .resizable()
+                                                .frame(width: 104, height: 72)
+                                                .clipShape(Capsule())
+                                                .overlay(
+                                                    Capsule()
+                                                        .strokeBorder(.white, lineWidth: 1)
+                                                )
+
+                                            VStack(alignment: .leading) {
+                                                Text(crewMember.astronaut.name)
+                                                    .foregroundColor(.white)
+                                                    .font(.headline)
+                                                Text(crewMember.role)
+                                                    .foregroundColor(.secondary)
+                                            }
+                                        }
+                                        .padding(.horizontal)
+                                    }
+                                }
+                            }
+                        }
+                        // Custom Divider
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                         
                         Text(mission.description)
+                        // Another Divider to show end of description
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
                     }
                     .padding(.horizontal)
                 }
@@ -40,9 +111,10 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
 }
