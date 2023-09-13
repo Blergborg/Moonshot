20ee5ab631ba6c0de1cdb7f28fc863a33e26c2ae
Blergborg <pprater92@gmail.com>
2023-09-13 09:47:11 -0700

    Added example of basic NavigationLink use

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        /*
+         So, both sheet() and NavigationLink allow us to show a new view from the current one, but the way they do it is different and you should choose them carefully:
+
+             NavigationLink is for showing details about the user’s selection, like you’re digging deeper into a topic.
+         
+             sheet() is for showing unrelated content, such as settings or a compose window.
+
+         */
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink {
+                    Text("Detail \(row)")
+                } label: {
+                    Text("Row \(row)")
+                }
+            }
+            .navigationTitle("SwiftUI")
         }
-        .padding()
     }
 }
 
