676dcf40bcd375a1a2b7a25dfd8fb076aec2afce
Blergborg <pprater92@gmail.com>
2023-09-13 13:15:34 -0700

    Updated Mission struct to have launchDate be type Date? and have computed properties for returning formatted name, making proper image string, and formatting launch date (or giving N/A if launch date is nil). Added formatter to Bundle-Decodable to set the date format for the JSONDecoder(). Added Color-Theme.swift to extend	ShapeStyle and define names and settings for custom colors. Updated layout of ContentView to use a scrolling grid of mission NavigationLinks that contain each mission's logo and text elements for name and launch date. Added clipShape, padding, new colors from Color-Theme, and set view to prefer the .dark color scheme.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     // NOTE: since Bundle decode() extension now uses generic we need to specify types for astronauts and missions.
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("\(astronauts.count)")
-            Text("\(missions.count)")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
-        .padding()
     }
 }
 
